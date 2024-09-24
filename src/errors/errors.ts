12223b8b2b0aeb971098e1a9f84b2979b567c36c
hp-qwlt <haripriya@qwlt.io>
2024-09-24 16:36:43 +0530

    added default status

--- src/errors/errors.ts
+++ src/errors/errors.ts
@@ -3,12 +3,14 @@
 export class KnowledgeKeeperError extends Error {
     public statusCode: number;
     public errorCode: string;
+    public status: string;
 
     constructor(errorObj: { HTTP_STATUS: number; ERROR_CODE: string; ERROR_MESSAGE: string }) {
         super(errorObj.ERROR_MESSAGE);
         this.statusCode = errorObj.HTTP_STATUS;
         this.errorCode = errorObj.ERROR_CODE;
         this.name = 'KnowledgeKeeperError';
+        this.status = 'failed';
         Error.captureStackTrace(this, this.constructor);
     }
 }
