cb745d39f484f04676712b6e03653ad6fef4654d
hp-qwlt <haripriya@qwlt.io>
2024-10-15 15:56:50 +0530

    added migration

--- src/db/migration/1728987642238-data_source.ts.ts
+++ src/db/migration/1728987642238-data_source.ts.ts
@@ -4,15 +4,15 @@ export class DataSourcets1728987642238 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
             CREATE TABLE user_teamspace (
-                id INT PRIMARY KEY AUTO_INCREMENT,
+                id SERIAL PRIMARY KEY,
                 "userId" INT NOT NULL,
                 "teamspaceId" INT NOT NULL,
-                status ENUM('INVITED', 'ACCEPTED', 'REJECTED') DEFAULT 'INVITED',
+                status VARCHAR(255) DEFAULT NULL,
                 role VARCHAR(255) DEFAULT 'MEMBER',
                 "createdAt" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                "updatedAt" TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-                CONSTRAINT FK_user FOREIGN KEY ("userId") REFERENCES user_details(id) ON DELETE CASCADE,
-                CONSTRAINT FK_teamspace FOREIGN KEY ("teamspaceId") REFERENCES teamspace(id) ON DELETE CASCADE
+                "updatedAt" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                CONSTRAINT FK_user FOREIGN KEY ("userId") REFERENCES user_details(id),
+                CONSTRAINT FK_teamspace FOREIGN KEY ("teamspaceId") REFERENCES teamspace(id)
             );
         `);
     }
